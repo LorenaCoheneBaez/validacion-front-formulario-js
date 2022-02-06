d060358771f959dc0fea90590438ea94e471f1d7
LorenaCoheneBaez <loreley.cb15@gmail.com>
2022-02-06 01:09:57 -0300

    Corrijo estilo en home

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Digital Movies' });
+  res.render('index.ejs', { title: 'Digital Movies' });
 });
 
 module.exports = router;
